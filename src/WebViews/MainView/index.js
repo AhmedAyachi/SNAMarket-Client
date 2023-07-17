d80709b4f2793f36f65ef6ac1cdef6d6ba953572
AhmedAyachi <aayachi032@gmail.com>
2023-07-17 10:13:52 +0100

    init

---
+++ src/WebViews/MainView/index.js
@@ -0,0 +1,19 @@
+import "../index";
+import MainView from "./MainView";
+import {makeStatusBarTranslucent} from "resources";
+import {store} from "../../Store";
+import WebViews from "../WebViews";
+
+
+function onDeviceReady(){
+    makeStatusBarTranslucent();
+    WebView.defineWebViews(WebViews);
+    WebView.initiateStore(store,()=>{
+        MainView({parent:document.body});
+    });
+};
+
+document.addEventListener("deviceready",onDeviceReady,false);
+document.addEventListener("backbutton",()=>{
+    location.hash?history.back():window.plugins.appMinimize.minimize();
+},false);
